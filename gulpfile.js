1b84dd30435cff2d6248cd7169c3a72be4f9f684
logic-fabric <lowic@logic-fabric.fr>
2021-06-17 10:56:48 +0200

    devtool: add Gulp and gulp-sass

---
+++ gulpfile.js
@@ -0,0 +1,20 @@
+"use strict";
+
+const gulp = require("gulp");
+
+const sass = require("gulp-sass");
+sass.compiler = require("node-sass");
+
+function watch() {
+  gulp.watch("./css/scss/**/*.scss", buildCSS);
+}
+
+function buildCSS() {
+  return gulp
+    .src("./css/scss/main.scss")
+    .pipe(sass({ outputStyle: "compressed" }).on("error", sass.logError))
+    .pipe(gulp.dest("./css"));
+}
+
+module.exports.watch = watch;
+module.exports.buildCSS = buildCSS;
